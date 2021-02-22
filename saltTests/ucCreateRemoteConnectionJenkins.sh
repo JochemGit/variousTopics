6db0dbb513390142fcca4c4f3c4793e1dc97110b
Juser <juser@ansible.localdomain>
2021-02-22 13:59:41 +0100

    fix path

--- saltTests/ucCreateRemoteConnectionJenkins.sh
+++ saltTests/ucCreateRemoteConnectionJenkins.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 . /home/juser/.nvm/nvm.sh
-newman run ~juser/MQ\ remote\ connection\ with\ Salt\ Orchestrator.postman_collection.json -e ~juser/Notebook\ Salt\ test\ environment.postman_environment.json -g ~juser/My\ Workspace.postman_globals.json --insecure
+newman run ./MQ\ remote\ connection\ with\ Salt\ Orchestrator.postman_collection.json -e ./Notebook\ Salt\ test\ environment.postman_environment.json -g ./My\ Workspace.postman_globals.json --insecure
