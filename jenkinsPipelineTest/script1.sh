fc77ea0e533bb100bd03ccc494b409dfe6f27c9c
Juser <juser@host.local>
2021-04-14 11:55:19 +0200

    Test

--- jenkinsPipelineTest/script1.sh
+++ jenkinsPipelineTest/script1.sh
@@ -2,6 +2,9 @@
 
 scriptname=$(basename $0)
 wert1=$(uuidgen -t)
+echo "#######################"
+echo "JENKINS_HOME: ${JENKINS_HOME:-None}"
+echo "#######################"
 echo "$scriptname: Create QM1"
 echo "$scriptname: $wert1"
 exit 0
